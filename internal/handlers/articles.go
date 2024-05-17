5b58c6491a1860540aa5719844805ca350b071b3
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2024-05-16 22:25:35 -0400

    json encoding

--- internal/handlers/articles.go
+++ internal/handlers/articles.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+    "log"
 
 	"github.com/theloosygoose/cms-api/internal/db"
 )
@@ -14,9 +16,14 @@ func (h *ArticleHandler) HandleGetArticle(w http.ResponseWriter, r *http.Request
     results, err := h.db.DbGetArticles()
     if err != nil {
         w.WriteHeader(http.StatusInternalServerError)
+        return
     }
 
-    w.WriteHeader(http.StatusOK)
+    w.Header().Set("Content-Type", "application/json")
+    err = json.NewEncoder(w).Encode(results)
 
-    w.Write([]byte(results))
+    if err != nil {
+        log.Println("Unable to Encode Reponse to JSON")
+        return
+    }
 }
